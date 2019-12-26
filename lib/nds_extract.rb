f301d86f6a58ff2e7330c7b2c58095116daadae1
Jin Wu <jinn.wuu@gmail.com>
2019-12-26 20:25:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,17 +2,20 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 
-def director_totals(nds)
-  totals = {}
+def directors_totals(nds)
   director_index = 0
+  totals = {}
+
   while director_index < nds.length do
-    director = nds[director_index][:name]
-    totals[director] = 0
+    director_name = nds[director_index][:name]
+    totals[director_name] = 0
     movie_index = 0
+
     while movie_index < nds[director_index][:movies].length do
-      totals[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
+      totals[director_name] += nds[director_index][:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
+
     director_index += 1
   end
   totals
