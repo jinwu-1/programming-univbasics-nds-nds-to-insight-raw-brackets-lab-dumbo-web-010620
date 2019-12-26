91e269c301ae9029753f09c35e8d70802163019a
Jin Wu <jinn.wuu@gmail.com>
2019-12-26 20:12:59 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -17,24 +17,3 @@ def director_totals(nds)
   end
   total
 end
-
-
-
-
-
-
-def directors_totals(nds)
-  total = {}
-  director_index = 0
-  while director_index < nds.length do
-    director = nds[director_index][:name]
-    movie_index = 0
-    total[director] = 0
-    while movie_index < nds[director_index][:movies].length do
-      total[director] += nds[director_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1
-    end
-    director_index += 1
-  end
-  total
-end
