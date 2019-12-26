0943cee700eb988f34d44b791d1f870fa6327d39
Jin Wu <jinn.wuu@gmail.com>
2019-12-26 20:12:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,28 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+
+def director_totals(nds)
+  total = {}
+  director_index = 0
+  while director_index < nds.length do
+    director = nds[director_index][:name]
+    total[director] = 0
+    movie_index = 0
+    while movie_index < nds[director_index][:movies].length do
+      total[director] += nds[director][:movies][movie_index][:worldwide_gross]
+      movie_index += 1
+    end
+    director_index += 1
+  end
+  total
+end
+
+
+
+
+
+
 def directors_totals(nds)
   total = {}
   director_index = 0
